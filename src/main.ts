8f5f4a83dc364afc0a58fbe144ef60aa10e7d0a7
Vitor <www.joaovitorgravata@gmail.com>
2025-11-16 16:07:39 -0300

    Front End

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app'; // <-- Mude 'App' para 'AppComponent' e o caminho
+import { AppComponent } from './app/app'; 
 
-bootstrapApplication(AppComponent, appConfig) // <-- Mude 'App' para 'AppComponent'
+bootstrapApplication(AppComponent, appConfig) 
   .catch((err) => console.error(err));
